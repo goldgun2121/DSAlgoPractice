ef33be32908836c6b49979f00093808f88f5fc74
likhithkvvr <likhithkvvr@gmail.com>
2024-07-13 16:59:10 -0500

    valid parenthesis problem

--- Likhith_Practice/stack_valid.py
+++ Likhith_Practice/stack_valid.py
@@ -13,15 +13,38 @@ class Solution(object):
         if len(s) < 2:
             return False
         
-        L=0
-        
         for R in range(len(s)):
             if s[R] in newChar.values():
                 newList.append(s[R])
             else:
-                if newChar[s[R]] not in newList:
+                if len(newList) < 1:
                     return False
+                else:
+                    if newChar[s[R]] != newList.pop():
+                        return False
+        if len(newList) != 0:
+            return False
         return True
+ 
+ 
+#  code observed
+    def isValidObserved(self, s):
+        
+        newChar = {')': '(',
+                   '}': '{',
+                   ']': '['}
+        
+        stack = []
+        
+        for c in s:
+            if c in newChar:
+                if stack and stack[-1] == newChar[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
                 
 solution = Solution()
-print(solution.isValid("{[]}"))
+print(solution.isValid("){"))
